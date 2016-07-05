398de11cfac8b3371b068a457d8fcd1d5d5a1d96
Amir Moulavi <amir.moulavi@klarna.com>
2016-07-05 20:47:05 +0200

    [#17]: Moved type checking into its own project under lib/type_checker

--- lib/compiler/src/type_macros.hrl
+++
@@ -1,6 +0,0 @@
--define(INTEGER, {terl_type, integer}).
--define(FLOAT, {terl_type, float}).
--define(BOOLEAN, {terl_type, boolean}).
--define(STRING, {terl_type, string}).
--define(INVALID, type_internal:invalid_operator()).
--define(UNDEFINED, undefined).
