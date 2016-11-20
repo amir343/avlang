b5b401a87be3e31f66a2acd9288ace4b00d657c8
Amir Moulavi <amir.moulavi@gmail.com>
2016-11-20 22:04:55 +0100

    More work

--- make/common_targets.mk
+++ make/common_targets.mk
@@ -1,2 +1,14 @@
 
 ERLC = $(TERL_ROOT)/bootstrap/bin/erlc
+SRC = src
+EBIN = ebin
+EGEN = ebin
+
+$(EBIN)/%.erl: $(SRC)/%.yrl
+	$(ERLC) $(YRL_FLAGS) -o$(EBIN) $<
+
+$(EBIN)/%.erl: $(SRC)/%.xrl
+	$(ERLC) $(XRL_FLAGS) -o$(EBIN) $<
+
+$(EBIN)/%.beam: $(SRC)/%.erl
+	$(ERLC) $(ERL_COMPILE_FLAGS) -o$(EBIN) $<
